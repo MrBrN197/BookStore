b72c9d6ab3d7a674be2668a0e2b1a0e77214389f
brianorora <brianorora@gmail.com>
2021-11-02 01:07:34 +0300

    Fix: remove `import Categories...`

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
-import Categories from './components/Categories';
 
 ReactDOM.render(
   <React.StrictMode>
