e7d81029256d372e3e81b436938b32ea9b21a0c4
brianorora <brianorora@gmail.com>
2021-11-02 22:13:12 +0300

    Add `AddBook` component `title` & `genre` state

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,16 +1,33 @@
-const AddBook = () => (
-  <div>
-    <h2>ADD NEW BOOK</h2>
-    <form onSubmit={(e) => e.preventDefault()}>
-      <input type="text" placeholder="Book title" />
-      <select name="genre">
-        <option value="action">Action</option>
-        <option value="science-fiction">Science Fiction</option>
-        <option value="economy">Economy</option>
-      </select>
-      <input type="submit" value="ADD BOOK" />
-    </form>
-  </div>
-);
+import { useState } from 'react';
+
+const AddBook = () => {
+  const [title, setTitle] = useState('');
+  const [genre, setGenre] = useState('');
+  console.log(title, genre);
+  return (
+    <div>
+      <h2>ADD NEW BOOK</h2>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          placeholder="Book title"
+        />
+        <select
+          name="genre"
+          value={genre}
+          onChange={(e) => setGenre(e.target.value)}
+        >
+          <option hidden value="">-- Category --</option>
+          <option value="action">Action</option>
+          <option value="science-fiction">Science Fiction</option>
+          <option value="economy">Economy</option>
+        </select>
+        <input type="submit" value="ADD BOOK" />
+      </form>
+    </div>
+  );
+};
 
 export default AddBook;
